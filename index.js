4655e4e99b6a9413bcd96c7d05be5a79eae83b1d
Nikita Kukharchuk <nikitakukharchuk@mail.ru>
2022-06-24 17:26:45 +0300

    init jwt+express+mongoose

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-let a = 5;
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+mongoose
+  .connect(
+    "mongodb+srv://nikitakukharchuk:www@cluster0.hmyqiiy.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("Db error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Вася Пупкин",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});

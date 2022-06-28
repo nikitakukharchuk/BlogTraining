b431ed14541197a47bccf9ed3fb0179e642fa1a0
Nikita Kukharchuk <nikitakukharchuk@mail.ru>
2022-06-28 15:56:07 +0300

    init (registerValidation) : blogTraining

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
 mongoose
   .connect(
     "mongodb+srv://nikitakukharchuk:www@cluster0.hmyqiiy.mongodb.net/?retryWrites=true&w=majority"
@@ -12,24 +14,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Вася Пупкин",
-    },
-    "secret123"
-  );
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
   res.json({
     success: true,
-    token,
   });
 });
 

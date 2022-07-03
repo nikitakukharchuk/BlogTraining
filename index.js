aedf547488c11b0e8f93db1161edf4cd19cfc91e
Nikita Kukharchuk <nikitakukharchuk@mail.ru>
2022-07-03 20:36:20 +0300

    init ( storage ) : trainingBlog

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidation, loginValidation } from "./validations.js";
+import multer from "multer";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -13,13 +19,28 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
 
 app.post("/auth/login", loginValidation, UserController.login);
-
 app.post("/auth/register", registerValidation, UserController.register);
-
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
